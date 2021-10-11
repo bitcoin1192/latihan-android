c579550d54cec571b75b73958287c327b0b145b1
Ariq <bitcoin1192@gmail.com>
2021-10-11 17:27:45 +0700

    update resource folder and gradle file

---
+++ app/src/main/java/com/sisalma/movieticketapp/onboarding/OnboardingOneActivity.kt
@@ -0,0 +1,31 @@
+package com.sisalma.movieticketapp.onboarding
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.sisalma.movieticketapp.R
+import com.sisalma.movieticketapp.activity_sign_in
+
+class OnboardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_one)
+
+        var next = findViewById<Button>(R.id.buttonTrue)
+        var skip = findViewById<Button>(R.id.button_false)
+
+        next.setOnClickListener(){
+            var intent = Intent(this, OnboardingTwoActivity::class.java)
+            startActivity(intent)
+        }
+
+        skip.setOnClickListener(){
+            var intent = Intent(this, activity_sign_in::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/sisalma/movieticketapp/onboarding/OnboardingThirdActivity.kt
@@ -0,0 +1,24 @@
+package com.sisalma.movieticketapp.onboarding
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.sisalma.movieticketapp.R
+import com.sisalma.movieticketapp.activity_sign_in
+
+class OnboardingThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_third)
+
+        var next = findViewById<Button>(R.id.buttonTrue)
+
+        next.setOnClickListener(){
+            finishAffinity()
+            var intent = Intent(this, activity_sign_in::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
