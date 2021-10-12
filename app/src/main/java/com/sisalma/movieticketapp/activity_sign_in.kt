814bf3ee9dc8f1778995fc516055c057b208022a
Ariq <bitcoin1192@gmail.com>
2021-10-12 20:07:01 +0700

    Add sign up page and update button flow on sign in page

--- app/src/main/java/com/sisalma/movieticketapp/activity_sign_in.kt
+++ app/src/main/java/com/sisalma/movieticketapp/activity_sign_in.kt
@@ -20,13 +20,24 @@ class activity_sign_in : AppCompatActivity() {
         setContentView(R.layout.activity_sign_in)
         database = FirebaseDatabase.getInstance("https://latihan-mta-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("User")
         var next = findViewById<Button>(R.id.buttonTrue)
+        var btnDaftar = findViewById<Button>(R.id.buttonFalse)
         var user = findViewById<EditText>(R.id.inputUser)
         var pass = findViewById<EditText>(R.id.inputPass)
+
         next.setOnClickListener(){
             var textUser = user.text.toString()
             var textPass = pass.text.toString()
-            checkLoginData(textUser,textPass)
+            if(textUser.isEmpty() or textPass.isEmpty()){
+                Toast.makeText(this,"Username atau password belum terisi", Toast.LENGTH_LONG).show()
+            }else{
+                checkLoginData(textUser,textPass)
+            }
         }
+        btnDaftar.setOnClickListener(){
+            val intent = Intent(this, activity_sign_up::class.java)
+            startActivity(intent)
+        }
+
     }
 
     private fun checkLoginData(username:String, password:String):Int{
@@ -35,7 +46,7 @@ class activity_sign_in : AppCompatActivity() {
                 var user = dataSnapshot.getValue(Users::class.java)
                 if (user != null) {
                     if(password == user.password.toString()){
-                        val intent = Intent(this@activity_sign_in, MainActivity::class.java)
+                        val intent = Intent(this@activity_sign_in, activity_sign_up::class.java)
                         startActivity(intent)
                         finish()
                     }else{
