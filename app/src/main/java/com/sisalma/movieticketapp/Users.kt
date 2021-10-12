6a99cc3c464695ac9340d1b6c9eb7f768ac0a9b1
Ariq <bitcoin1192@gmail.com>
2021-10-12 09:43:43 +0700

    Update string to be nullable

---
+++ app/src/main/java/com/sisalma/movieticketapp/Users.kt
@@ -0,0 +1,10 @@
+package com.sisalma.movieticketapp
+
+class Users {
+    var email: String ?= ""
+    var nama: String ?= ""
+    var password: String ?= ""
+    var saldo: String ?= ""
+    var url: String ?= ""
+    var username: String ?= ""
+}
