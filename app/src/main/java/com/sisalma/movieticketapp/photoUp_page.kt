3e053e5d9f40fc80badd29eacfd7378b7cccb6d3
Ariq <bitcoin1192@gmail.com>
2021-10-15 21:44:21 +0700

    Add profile photo changer page

---
+++ app/src/main/java/com/sisalma/movieticketapp/photoUp_page.kt
@@ -0,0 +1,75 @@
+package com.sisalma.movieticketapp
+
+import android.content.Intent
+import android.media.Image
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.LogPrinter
+import android.view.View
+import android.widget.*
+import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import java.util.logging.Logger
+
+
+class photoUp_page : AppCompatActivity() {
+
+    lateinit var database: DatabaseReference
+    lateinit var storage: FirebaseStorage
+    lateinit var storageRef: StorageReference
+    lateinit var filepath:Uri
+
+    val requst_image = 1
+    var statusAdd:Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.photoUp_page)
+        //Set database reference to latihan-mta/User firebase "https://latihan-mta-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        database = FirebaseDatabase.getInstance().getReference("User")
+        storageRef  = FirebaseStorage.getInstance().getReference("Photos")
+        //I wish this variable can be found automatically
+        var btnBack = findViewById<ImageView>(R.id.imageView4)
+        var next = findViewById<Button>(R.id.buttonTrue)
+        var btnAdd = findViewById<ImageView>(R.id.imageView5)
+
+        next.setOnClickListener(){
+
+        }
+        btnBack.setOnClickListener(){
+            //Move back to login activity
+            finish()
+        }
+        btnAdd.setOnClickListener(){
+            //If user is already adding image, make next button visible
+            //and show new image selected by user
+            //else, ask user to allow camera access
+            if(statusAdd){
+                statusAdd = false
+                next.visibility = View.VISIBLE
+
+            }
+        }
+    }
+    private fun saveUsertoFirebase(dataUser: Users){
+        database.child(dataUser.username).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var user = dataSnapshot.getValue(Users::class.java)
+                if (user != null) {
+                    Toast.makeText(this@photoUp_page, "Nama user "+ user.username + " tidak tersedia",Toast.LENGTH_LONG).show()
+                }else{
+                    // Update firebase to set new user then
+                    database.child(dataUser.username).setValue(dataUser)
+                    Toast.makeText(this@photoUp_page,"User berhasil dibuat", Toast.LENGTH_LONG).show()
+                }
+            }
+            override fun onCancelled(dbErr: DatabaseError) {
+                Toast.makeText(this@photoUp_page,dbErr.message,Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+}
