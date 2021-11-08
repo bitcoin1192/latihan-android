afcf69fba644accb3861b3b40889c9df1ee5db1b
Ariq <bitcoin1192@gmail.com>
2021-11-08 17:07:58 +0700

    Add home activity and home fragment. Enable viewBinding on build graddle

--- app/src/main/java/com/sisalma/movieticketapp/home/Home.kt
+++ app/src/main/java/com/sisalma/movieticketapp/home/Home.kt
@@ -2,11 +2,56 @@ package com.sisalma.movieticketapp.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import com.sisalma.movieticketapp.R
+import com.sisalma.movieticketapp.databinding.ActivityHomeBinding
 
 class home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        var binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /*val fragmentTiket = TiketFragment()
+        val fragmentSetting = SettingFragment()*/
+        val fragmentHome = dashboardFragment()
+
+        setFragment(fragmentHome)
+
+        binding.ivMenu1.setOnClickListener {
+            //setFragment(fragmentHome)
+
+            changeIcon(binding.ivMenu1, R.drawable.ic_home)
+            changeIcon(binding.ivMenu2, R.drawable.ic_tiket)
+            changeIcon(binding.ivMenu3, R.drawable.ic_profile)
+        }
+
+        binding.ivMenu2.setOnClickListener {
+            //setFragment(fragmentTiket)
+
+            changeIcon(binding.ivMenu1, R.drawable.ic_home)
+            changeIcon(binding.ivMenu2, R.drawable.ic_tiket)
+            changeIcon(binding.ivMenu3, R.drawable.ic_profile)
+        }
+
+        binding.ivMenu3.setOnClickListener {
+            //setFragment(fragmentSetting)
+
+            changeIcon(binding.ivMenu1, R.drawable.ic_home)
+            changeIcon(binding.ivMenu2, R.drawable.ic_tiket)
+            changeIcon(binding.ivMenu1, R.drawable.ic_profile)
+        }
+    }
+
+    protected fun setFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_frame, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun changeIcon(imageView: ImageView, int: Int) {
+        imageView.setImageResource(int)
     }
 }
